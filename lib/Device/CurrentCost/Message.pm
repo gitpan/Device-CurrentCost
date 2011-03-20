0fe34b92218b6082f3b2e556aeefe7c5e6329be5
Mark Hindess <soft-cpan@temporalanomaly.com>
2011-03-20 13:03:31 +0000

    Import of BEANZ/Device-CurrentCost-1.110791 from CPAN.
    gitpan-cpan-distribution: Device-CurrentCost
    gitpan-cpan-version:      1.110791
    gitpan-cpan-path:         BEANZ/Device-CurrentCost-1.110791.tar.gz
    gitpan-cpan-author:       BEANZ
    gitpan-cpan-maturity:     released

--- lib/Device/CurrentCost/Message.pm
+++ lib/Device/CurrentCost/Message.pm
@@ -2,7 +2,7 @@ use strict;
 use warnings;
 package Device::CurrentCost::Message;
 BEGIN {
-  $Device::CurrentCost::Message::VERSION = '1.110790';
+  $Device::CurrentCost::Message::VERSION = '1.110791';
 }
 
 # ABSTRACT: Perl modules for Current Cost energy monitor messages
@@ -224,7 +224,7 @@ Device::CurrentCost::Message - Perl modules for Current Cost energy monitor mess
 
 =head1 VERSION
 
-version 1.110790
+version 1.110791
 
 =head1 SYNOPSIS
 
